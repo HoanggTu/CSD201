5e0af086cf5b3c5bba74db963b49b16fc65fc7b7
HoangTu <tungo6457@gmail.com>
2025-10-30 16:28:29 +0700

    removelinklist2

--- PRJ03/src/prj03/Main.java
+++ PRJ03/src/prj03/Main.java
@@ -16,7 +16,28 @@ public class Main {
             list1.addLast(new Student("00" + i, "Nguyen Van " + i, i));
         }
         
+        list1.traverse();
+        System.out.println("=============");
+        list1.traverseReverse();
         
+        System.out.println("=================");
+        list1.removeFirst();
+        list1.traverse();
+        System.out.println("=================");
+        for (int i = 0; i < 5; i++) {
+            list1.removeFirst();
+        }
+        list1.traverse();
+        System.out.println("=================");
+        list1.removeLast();
+        list1.traverse();
+        System.out.println("=================");
+        list1.remove(4); // Xoa 009
+        list1.traverse();
+
+        System.out.println(list1.size());
+        list1.remove(4); // Xoa 010
+        System.out.println(list1.size());
     }
    
 }
--- PRJ03/src/prj03/MyDoubleLinkedList2.java
+++ PRJ03/src/prj03/MyDoubleLinkedList2.java
@@ -9,5 +9,48 @@ package prj03;
  * @author rechiee
  */
 public class MyDoubleLinkedList2 {
+    private Node head;
+    private Node tail;
+    private int count = 0;
+
+    public MyDoubleLinkedList2() {
+        this.head = null;
+        this.tail = null;
+        this.count = 0;
+    }
+    
+    public boolean isEmpty(){
+        return head == null && tail == null;
+    }
+    
+    public void addLast(Student st){
+        Node newNode = new  Node(st, null);
+        if(isEmpty()){
+            this.head= newNode;
+            this.tail= newNode;
+        }else{
+            this.tail.next= newNode;
+            newNode.prev=newNode;
+            
+            this.tail = newNode;
+        }
+        count++;
+    }
+    
+    public void addFrist(){
+        if(this.isEmpty()){
+            return;
+        }else{
+            new
+            
+            
+        }
+    }
+    
+     
+
+    
+    
+    
     
 }
